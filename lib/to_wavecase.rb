d169f2a9149b6cf61f9eca33e3c5702abee4ae02
Ezzy Elliott <ezzy.elliott@gmail.com>
2015-12-04 11:14:46 +0000

    Answer

--- lib/to_wavecase.rb
+++ lib/to_wavecase.rb
@@ -1,4 +1,24 @@
 class String
 
+  def to_wavecase
+    results =''
+    count=0
+    self.chars.each do |c|
+      if c==" "
+        results+=" "
+        count+=1
+      else
+        if count.even?
+          results+= c.downcase
+        else
+          results+=c.upcase
+        end
+      end
+      count+=1
+    end
+    results
+  end
+
 
 end
+
